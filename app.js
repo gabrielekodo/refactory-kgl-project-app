bd5c79e738a20decd7131cd35438b230f1f69ce8
Gabriel Ekodo <ekodogabriel@outlook.com>
2022-06-19 16:02:32 +0300

    updated css

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 // selecting elements
-const loginForm=document.querySelector('form')
-
+const regBtn = document.getElementById("reg-btn");
+const loginForm = document.getElementById("login");
+const regForm = document.getElementById("register");
 //event functions
-const login=(e)=>{
-    e.preventDefault()
-    let email=document.getElementById('inputEmail3').value
-    let password=document.getElementById('inputPassword3').value
+const login = (e) => {
+  e.preventDefault();
+  let email = document.getElementById("inputEmail3").value;
+  let password = document.getElementById("inputPassword3").value;
 
-    if(email !=='' && password !==''){
-        location.href='/manager-dashboard.html'
-    }
-    else{
-        alert('incorrect email or password!')
-    }
-}
+  if (email !== "" && password !== "") {
+    location.href = "manager-dashboard.html";
+  } else {
+    alert("incorrect email or password!");
+  }
+};
 
+const register = (e) => {
+  e.preventDefault();
+  alert("clicked----");
+};
+
+const toggleReg = () => {
+  document.querySelector("#login-wrapper").classList.add("d-none");
+  document.querySelector("#reg-wrapper").classList.remove("d-none");
+  // document.querySelector("#login-wrapper").classList.add("d-none");
+  // document.querySelector("#login-wrapper").style.tranform = "translate(0%)";
+  // document.querySelector("#login-wrapper").style.left = "200%";
+};
 
 //event listeners
-loginForm.addEventListener('submit',login)
+loginForm.addEventListener("submit", login);
+regForm.addEventListener("submit", register);
+regBtn.addEventListener("click", toggleReg);
